3d6d6ff3a686e855048a37e2a3584e7bc22364bd
Ravelo-Dev <zmarcos140@gmail.com>
2024-10-01 09:43:26 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,18 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'tabla-de-multiplicar',
+    loadChildren: () => import('./tabla-de-multiplicar/tabla-de-multiplicar.module').then( m => m.TablaDeMultiplicarPageModule)
+  },
+  {
+    path: 'numeros',
+    loadChildren: () => import('./numeros/numeros.module').then( m => m.NumerosPageModule)
   }
 ];
 
