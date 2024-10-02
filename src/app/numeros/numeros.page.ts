e844d9a8120d48760264cd1671e8ea75c27f034c
Ravelo-Dev <zmarcos140@gmail.com>
2024-10-02 07:48:43 -0400

    Practica casi terminada

--- src/app/numeros/numeros.page.ts
+++ src/app/numeros/numeros.page.ts
@@ -8,6 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class NumerosPage implements OnInit {
 
   constructor() { }
+  numero1: number = 0;
+  numero2: number = 0;
+  numero3: number = 0;
+
+  mayor: number = 0;
+  menor: number= 0;
+  promedio: number = 0;
+
+  calcular(){
+    const numeros = [this.numero1, this.numero2, this.numero3];
+       this.mayor = Math.max(...numeros);
+       this.menor = Math.min(...numeros);
+       this.promedio = numeros.reduce((a, b) => a + b, 0) / numeros.length;
+  }
 
   ngOnInit() {
   }
